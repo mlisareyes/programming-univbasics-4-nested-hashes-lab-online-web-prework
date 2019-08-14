bebeda924d02073662f18a9bbc9a8f690c6537e3
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-14 20:51:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[hash.to_a.unshift([k, v])]
+programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
+programmer_hash
     
 end
 
